5fbfca75cd9b4bfe410b2a628ebce6569a6363f8
Pascal <c28g2z88wt@privaterelay.appleid.com>
2026-07-16 17:29:43 +0200

    Update dwin.cpp

--- Marlin/src/lcd/dwin/creality/dwin.cpp
+++ Marlin/src/lcd/dwin/creality/dwin.cpp
@@ -23,12 +23,6 @@ lin 3D Printer Firmware
  *DWIN by Creality3D
  */
 
--// Example problematic global (original)
--const char CZ_AFTER_HOMING[] = "some string or value";
-+// Make this file-local to avoid multiple-definition at link time
-+static const char CZ_AFTER_HOMING[] = "some string or value";
-*** End Patch
-
 #include "../../../inc/MarlinConfigPre.h"
 #include "../../../inc/MarlinConfig.h"
 #if ENABLED(DWIN_CREALITY_LCD)
